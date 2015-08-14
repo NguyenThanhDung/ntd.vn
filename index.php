c0b8ef265c799405824371e777c5fd934cf9c8bf
NguyenThanhDung <thanhdungspk@yahoo.com.vn>
2015-08-14 22:29:40 +0700

    + Create index page with gradient black background

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Nguyễn Thành Dũng's Website</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+
+<body>
+
+
+
+</<body>
+
+</html>
