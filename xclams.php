8bc5752bd7ac3bfa813d3f674b098580241b5bcb
unknown <dung.nguyenthanh@sa2wks0080.sa2.gameloft.org>
2015-08-27 17:58:06 +0700

    + Add comments

--- xclams.php
+++ xclams.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 
+<?php
+$wasLogin = false;
+?>
+
 <html>
 
 <head>
@@ -25,50 +29,74 @@
 	
 		<div class="entry">			
 			<div class="datetime">
-				<p>Wednesday<br/>Aug 26th, 2015</p>
+				<p>8:27 AM, Wednesday<br/>Aug 26th, 2015</p>
 			</div>			
 			<div class="content">
 				<p>W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding.</p>
 				<p>Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. Copyright 1999-2015 by Refsnes Data. All Rights Reserved.</p>
-			</div>	
-			<div class="comment_list">
+			</div>
+			<div class="comments">
 				<p><span class="user">User 1:</span> This is a comment</p>
 				<p><span class="user">User 2:</span> This is a comment</p>
-				<form>
-					<input type="text" name="comment_content" value="Write a comment...">
-					<input type="submit" value="Send">
-				</form>
 			</div>
+			<?php if($wasLogin)	{ ?>
+				<div class="comment_form">
+					<form>
+						<input type="text" name="comment_content" value="Write a comment...">
+						<input type="submit" value="Send">
+					</form>
+				</div>
+			<?php } else {	?>
+				<div class="comment_link">
+					<a href="login.php">Comment</a>
+				</div>
+			<?php } ?>
 		</div>
 	
 		<div class="entry">			
 			<div class="datetime">
-				<p>Wednesday<br/>Aug 26th, 2015</p>
+				<p>10:32 AM, Wednesday<br/>Aug 26th, 2015</p>
 			</div>			
 			<div class="content">
 				<p>Used together with the direction property to set or return whether the text should be overridden to support multiple languages in the same document</p>
-			</div>				
-			<div class="comment_list">
-				<form>
-					<input type="text" name="comment_content" value="Write a comment...">
-					<input type="submit" value="Send">
-				</form>
 			</div>
+			<div class="comments">
+			</div>
+			<?php if($wasLogin)	{ ?>
+				<div class="comment_form">
+					<form>
+						<input type="text" name="comment_content" value="Write a comment...">
+						<input type="submit" value="Send">
+					</form>
+				</div>
+			<?php } else {	?>
+				<div class="comment_link">
+					<a href="login.php">Comment</a>
+				</div>
+			<?php } ?>
 		</div>
 	
 		<div class="entry">			
 			<div class="datetime">
-				<p>Wednesday<br/>Aug 26th, 2015</p>
+				<p>2:08 PM, Wednesday<br/>Aug 26th, 2015</p>
 			</div>			
 			<div class="content">
 				<p>Increases or decreases the space between words in a text</p>
-			</div>				
-			<div class="comment_list">
-				<form>
-					<input type="text" name="comment_content" value="Write a comment...">
-					<input type="submit" value="Send">
-				</form>
+			</div>		
+			<div class="comments">
 			</div>
+			<?php if($wasLogin)	{ ?>
+				<div class="comment_form">
+					<form>
+						<input type="text" name="comment_content" value="Write a comment...">
+						<input type="submit" value="Send">
+					</form>
+				</div>
+			<?php } else {	?>
+				<div class="comment_link">
+					<a href="login.php">Comment</a>
+				</div>
+			<?php } ?>
 		</div>
 		
 	</div>
