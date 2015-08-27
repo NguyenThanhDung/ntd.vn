8eb6da0af61f4ce5a1dde8f07a3f5443b09c07e8
unknown <dung.nguyenthanh@sa2wks0080.sa2.gameloft.org>
2015-08-27 11:32:20 +0700

    - Align

--- xclams.php
+++ xclams.php
@@ -25,12 +25,12 @@
 	
 		<div class="entry">			
 			<div class="datetime">
-				<p>Wednesday</p>
-				<p>Aug 26th, 2015</p>
+				<p>Wednesday<br/>Aug 26th, 2015</p>
 			</div>			
 			<div class="content">
-				<p>W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. Copyright 1999-2015 by Refsnes Data. All Rights Reserved.</p>
-			</div>			
+				<p>W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding.</p>
+				<p>Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. Copyright 1999-2015 by Refsnes Data. All Rights Reserved.</p>
+			</div>	
 			<div class="comment_list">
 				<p><span class="user">User 1:</span> This is a comment</p>
 				<p><span class="user">User 2:</span> This is a comment</p>
@@ -43,22 +43,32 @@
 	
 		<div class="entry">			
 			<div class="datetime">
-				<p>Wednesday</p>
-				<p>Aug 26th, 2015</p>
+				<p>Wednesday<br/>Aug 26th, 2015</p>
 			</div>			
 			<div class="content">
-				<p>Hello World!</p>
-			</div>			
+				<p>Used together with the direction property to set or return whether the text should be overridden to support multiple languages in the same document</p>
+			</div>				
+			<div class="comment_list">
+				<form>
+					<input type="text" name="comment_content" value="Write a comment...">
+					<input type="submit" value="Send">
+				</form>
+			</div>
 		</div>
 	
 		<div class="entry">			
 			<div class="datetime">
-				<p>Wednesday</p>
-				<p>Aug 26th, 2015</p>
+				<p>Wednesday<br/>Aug 26th, 2015</p>
 			</div>			
 			<div class="content">
-				<p>Hello World!</p>
-			</div>			
+				<p>Increases or decreases the space between words in a text</p>
+			</div>				
+			<div class="comment_list">
+				<form>
+					<input type="text" name="comment_content" value="Write a comment...">
+					<input type="submit" value="Send">
+				</form>
+			</div>
 		</div>
 		
 	</div>
