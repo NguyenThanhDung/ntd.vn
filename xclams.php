588e31ab10e5d7415fa69faf1d505e1e8ccd5094
Nguyen Thanh Dung <thanhdungspk@yahoo.com.vn>
2017-03-05 21:47:23 +0700

    - Add utils class for debuggin

--- xclams.php
+++ xclams.php
@@ -5,12 +5,13 @@
 
 // TODO:
 // - Confirm with user before delete xclam or comment
-// - Hide the suggestion text in the text input when user click on it 
+// - Hide the suggestion text in the text input when user click on it
 // - Display Xclams by page number
 // - Adjust user inteface for mobile device
 // - Validate the user email address
 
 include "config.php";
+include "utils.php";
 include "objects/User.php";
 include "objects/Xclam.php";
 include "objects/Comment.php";
@@ -49,7 +50,7 @@ if($loggedUser)
 				XclamManager::PostXclam($_POST["content"]);
 			}
 			break;
-			
+
 		case ACTION_EDIT_XCLAM_FORM:
 			if($loggedUser->GetType() == UserType::ADMIN)
 			{
@@ -61,7 +62,7 @@ if($loggedUser)
 				}
 			}
 			break;
-			
+
 		case ACTION_SUBMIT_EDITED_XCLAM:
 			if($loggedUser->GetType() == UserType::ADMIN)
 			{
@@ -73,7 +74,7 @@ if($loggedUser)
 				}
 			}
 			break;
-			
+
 		case ACTION_DELETE_XCLAM:
 			if($loggedUser->GetType() == UserType::ADMIN)
 			{
@@ -83,7 +84,7 @@ if($loggedUser)
 				}
 			}
 			break;
-			
+
 		case ACTION_POST_COMMENT:
 			$content = isset($_POST["content"]) ? $_POST["content"] : false;
 			$entryId = isset($_POST["entry_id"]) ? $_POST["entry_id"] : false;
@@ -92,27 +93,27 @@ if($loggedUser)
 				CommentManager::PostComment($loggedUser, $content, EntryType::XCLAM, $entryId);
 			}
 			break;
-			
+
 		case ACTION_EDIT_COMMENT_FORM:
 			$id = $_GET["id"];
 			$editingComment = CommentManager::GetCommentById($id);
 			$includedPage = "edit_comment";
 			break;
-			
+
 		case ACTION_SUBMIT_EDITED_COMMENT:
 			$id = $_POST["id"];
 			$content = $_POST["content"];
 			$entryId = $_POST["entry_id"];
 			CommentManager::UpdateComment($id, $loggedUser, $content, EntryType::XCLAM, $entryId);
 			break;
-			
+
 		case ACTION_DELETE_COMMENT:
 			if(isset($_GET["id"]))
 			{
 				CommentManager::DeleteComment($_GET["id"]);
 			}
 			break;
-			
+
 		default:
 			break;
 	}
